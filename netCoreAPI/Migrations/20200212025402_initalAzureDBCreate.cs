0987f3ae384ff77185760d6d29c743c328101612
Athena Kozak <athenakozak@gmail.com>
2020-02-13 14:50:18 -0800

    Created Azure SQL db and added migrations.

---
+++ netCoreAPI/Migrations/20200212025402_initalAzureDBCreate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace netCoreAPI.Migrations
+{
+    public partial class initalAzureDBCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
